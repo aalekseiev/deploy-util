ff940821c0906b3fa664594a0f9d157214c7eddc
kseniia <alekseyev.anton@gmail.com>
2017-11-01 17:42:01 +0200

    initial commit

---
+++ src/test/java/com/example/testtool/TestToolApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.testtool;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class TestToolApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
