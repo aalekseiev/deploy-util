495e684b7fc596936397561695a06326243dc806
kseniia <alekseyev.anton@gmail.com>
2017-11-03 09:18:58 +0200

    properties configs

--- src/main/java/com/example/testtool/DeployUtilApplication.java
+++ src/main/java/com/example/testtool/DeployUtilApplication.java
@@ -4,12 +4,14 @@ import java.util.Arrays;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 @SpringBootApplication
 public class DeployUtilApplication {
 	
 	public static void main(String[] args) {
 		System.out.println("APPLICATION ARGUMENTS: " + Arrays.toString(args));
+						
 		SpringApplication.run(DeployUtilApplication.class, args);
 	}
 }
